e09d96143161af597dfd2fa97dd42ec5b17209e9
zephyrxvxx7 <philosophyz.520@gmail.com>
2018-01-31 17:35:24 +0800

    Create FCU_grabbed_class.py

---
+++ FCU_grabbed_class.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import os
+from time import sleep
+
+
+mainUrl = "https://course.fcu.edu.tw"
+
+
+chrome_options = webdriver.ChromeOptions()
+
+browser = webdriver.Chrome(chrome_options=chrome_options)
+browser.maximize_window()
+
+# 帳號及密碼
+username = 'd0471777'
+password = ''
+
+# 選課代號
+classID = '1459'
+
+# 確定要搶課嗎？
+grabbed = True
+
+
+def login():
+    browser.get(mainUrl)
+
+    cookies = browser.get_cookies()
+    print('驗證碼:', cookies[2]['value'])
+    # alert = browser.switch_to_alert()
+    # print(alert.text)
+    # alert.accept()
+
+    browser.find_element_by_xpath(
+        '//*[@id="ctl00_Login1_UserName"]').send_keys(username)
+    browser.find_element_by_xpath(
+        '//*[@id="ctl00_Login1_Password"]').send_keys(password)
+    browser.find_element_by_xpath(
+        '//*[@id="ctl00_Login1_vcode"]').send_keys(cookies[2]['value'])
+    browser.find_element_by_xpath(
+        '//*[@id="ctl00_Login1_LoginButton"]').click()
+
+    print('登入成功')
+
+
+def grab():
+    while True:
+        if browser.current_url != mainUrl:
+            break
+    while True:
+        # select
+        browser.find_element_by_xpath(
+            '//*[@id="ctl00_MainContent_TabContainer1_tabSelected_Label3"]').click()
+
+        # input class ID
+        browser.find_element_by_xpath(
+            '//*[@id="ctl00_MainContent_TabContainer1_tabSelected_tbSubID"]').send_keys(classID)
+        browser.find_element_by_xpath(
+            '//*[@id="ctl00_MainContent_TabContainer1_tabSelected_gvToAdd"]/tbody/tr[2]/td[8]/input').click()
+
+        # alert
+        sleep(0.5)
+        alert = browser.switch_to_alert()
+
+        alertInfo = alert.text
+        currentValue = int(alertInfo[13:15])
+        openValue = int(alertInfo[21:23])
+        alert.accept()
+
+        print('目前人數:', currentValue)
+        print('開放人數:', openValue)
+
+        if(currentValue < openValue and grabbed):
+            break
+
+        browser.get(browser.current_url)
+
+    # 選課
+    if grabbed:
+        browser.find_element_by_xpath(
+            '//*[@id="ctl00_MainContent_TabContainer1_tabSelected_gvToAdd"]/tbody/tr[2]/td[1]/input').click()
+        print('選課成功')
+    sleep(10)
+
+
+if __name__ == "__main__":
+    login()
+    grab()
+    browser.close()
